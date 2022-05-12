836e8c37f976f056b9e0629dabbd234a5efa8f65
Andrey Kondratov <andrey.kondratov@ihsmarkit.com>
2022-05-13 01:15:41 +0300

    Added method body

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,15 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	inputRunes := []rune(input)
+	outputRunes := make([]rune, 0, len(inputRunes))
+
+	i := len(inputRunes) - 1
+	for i >= 0 {
+		outputRunes = append(outputRunes, inputRunes[i])
+		i--
+	}
+
+	output = string(outputRunes)
+	return
 }
